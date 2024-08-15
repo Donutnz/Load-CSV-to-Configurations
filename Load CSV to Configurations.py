1a5f604acf815699aa027f3ad6153a492b411ab9
Donutnz <josh118tb@gmail.com>
2024-08-15 12:00:02 +1200

    Added Insert Column Support

--- Load CSV to Configurations.py
+++ Load CSV to Configurations.py
@@ -68,40 +68,55 @@ def run(context):
                     app.log("Adding...")
 
                 for h,v in csvRow.items():
-                    for c in topTable.columns:
-                        if h == c.title:
-                            cellToBeSet=confRow.getCellByColumnId(c.id)
+                    for tColumn in topTable.columns:
+                        app.log("Column title: {}".format(tColumn.title))
+                        if h == tColumn.title:
+                            cellToBeSet=confRow.getCellByColumnId(tColumn.id)
 
                             if cellToBeSet is None:
                                 # If this fires, something is seriously weird.
-                                raise Exception("No cell found in at row: {} column: {}. \n Something really weird has happened.".format(confRow.name, c.title))
+                                raise Exception("No cell found in at row: {} column: {}. \n Something really weird has happened.".format(confRow.name, tColumn.title))
                             
-                            if isinstance(c, adsk.fusion.ConfigurationParameterColumn):
-                                # All parameters are numerical. I think.
+                            if isinstance(tColumn, adsk.fusion.ConfigurationParameterColumn): # All parameters are numerical. I think.
                                 try:
                                     cellToBeSet.expression=v
-                                except ValueError:
-                                    app.log("Failed to set parameter {} to {}".format(c.title, v))
+                                except RuntimeError:
+                                    app.log("Failed to set column {} to {}.".format(tColumn.title, v))
+                                    raise
                                 
                                 changesCnt+=1
-                            elif isinstance(c, adsk.fusion.ConfigurationThemeColumn): # Mostly aimed at material for now. Can overcomplicate later.
-                                for m in c.referencedTable.rows:
+
+                            elif isinstance(tColumn, adsk.fusion.ConfigurationThemeColumn): # Mostly aimed at material for now. Can overcomplicate later.
+                                for m in tColumn.referencedTable.rows:
                                     if v == m.name:
                                         cellToBeSet.referencedTableRow=m
                                         app.log("Set theme table row: {}".format(m.name))
                                         changesCnt+=1
-                            elif isinstance(c, adsk.fusion.ConfigurationPropertyColumn): # Description and part number
+
+                            elif isinstance(tColumn, adsk.fusion.ConfigurationPropertyColumn): # Description and part number
                                 #app.log("Property {}".format(h))
                                 cellToBeSet.value=v
+
                                 changesCnt+=1
-                            elif isinstance(c, adsk.fusion.ConfigurationSuppressColumn):
+
+                            elif isinstance(tColumn, adsk.fusion.ConfigurationSuppressColumn):
                                 if v == "TRUE":
                                     cellToBeSet.isSuppressed=True
                                 elif v == "FALSE":
                                     cellToBeSet.isSuppressed=False
                                 else:
-                                    raise TypeError("Supression type column input data must be TRUE or FALSE. {} column in the input CSV is not bool.".format(c.title))
+                                    raise TypeError("Supression type column input data must be TRUE or FALSE. {} column in the input CSV is not bool.".format(tColumn.title))
                                 
+                                changesCnt+=1
+
+                            elif isinstance(tColumn, adsk.fusion.ConfigurationInsertColumn): # Configuration insert. Most sketchy bit and most time inefficent. Should probs get the occurance top table just once.
+                                insTopTable:adsk.fusion.ConfigurationTopTable=tColumn.occurrence.configuredDataFile.configurationTable
+
+                                for tRow in insTopTable.rows:
+                                    if tRow.name == v:
+                                        #app.log("Inserted row {} from table {}".format(insTopTable.name, tRow.name))
+                                        cellToBeSet.row=tRow
+
                                 changesCnt+=1
                 if isUpdating:
                     app.log("Touched {} columns".format(changesCnt))
--- README.md
+++ README.md
@@ -4,11 +4,13 @@
 Reads a CSV containing simple variations and writes it into Fusion 360 Configurations rows.
 
 ## Usage
-Clone into your C:\Users\[YOUR_USERNAME]\AppData\Roaming\Autodesk\Autodesk Fusion 360\API\Scripts\Load CSV to Configurations folder.
+Clone into your C:\Users\\[YOUR_USERNAME]\AppData\Roaming\Autodesk\Autodesk Fusion 360\API\Scripts\Load CSV to Configurations folder.
 
 CSV must have headers. Headers must contain one "Part Number" column. This will set the row name and part number. See Sample Input file.
 
 ## Todo
 * Split Part Name and Part number up.
-* Change to Addin rather than a script with header to column mapping.
+* Change to Addin rather than a script with header to column mapping and GUI mapping.
+
+## Done
 * Add Insert column type support.
